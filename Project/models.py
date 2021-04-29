9e7ff4d2afffbc4d21fac020495ea6419aa15873
Szűcs Máté <szcsm2000@gmail.com>
2021-04-29 21:26:22 +0200

    continued pixelhnn

--- Project/models.py
+++ Project/models.py
@@ -28,13 +28,31 @@ class HNN(nn.Module):
                                   nn.Tanh(),
                                   nn.Linear(200, 1))
 
+        self.M = self.M_tensor(dims)
+
     def forward(self, x):
         out = self.model(x)
-
+        
         return out
 
+    def M_tensor(self, dims):
+      if dims == 1:
+        return torch.Tensor([[0, -1],[1, 0]])
+
+      elif dims == 2:
+        return torch.Tensor([[0, 0, 0, -1], [0, 0, -1, 0], [0, 1, 0, 0], [1, 0, 0, 0]])
+
+    def diff_hnn(self, x):
+      x = torch.autograd.Variable(x, requires_grad=True)
+
+      x_forward = self.forward(x)
+      out = torch.autograd.grad(x_forward.sum(), x, create_graph=True)[0]
+
+      return x_forward.sum(), out @ self.M
+
+
 # simple autoencoder
-class MLP_autoencoder(torch.nn.Module):
+class MLP_autoencoder(nn.Module):
   def __init__(self, dims):
     super(MLP_autoencoder, self).__init__()
     self.fcs1 = torch.nn.Linear(dims, 200)
@@ -47,27 +65,29 @@ class MLP_autoencoder(torch.nn.Module):
     self.fcs7 = torch.nn.Linear(200, 200)
     self.fcs8 = torch.nn.Linear(200, dims)
 
+    self.relu = nn.ReLU()
+
     def encode(self, x):
-    x = nn.Tanh((self.fcs1(x))
-    x = x + nn.Tanh(self.fcs2(x) 
-    x = x + nn.Tanh(self.fcs3(x))
-    return self.fcs4(x)
+      x = self.relu(self.fcs1(x))
+      x = x + self.relu(self.fcs2(x))
+      x = x + self.relu(self.fcs3(x))
+      return self.fcs4(x)
 
     def decode(self, x):
-    x = nn.Tanh((self.fcs5(x))
-    x = x + nn.Tanh(self.fcs6(x) 
-    x = x + nn.Tanh(self.fcs7(x))
-    return self.fcs8(x)
+      x = self.relu(self.fcs5(x))
+      x = x + self.relu(self.fcs6(x))
+      x = x + self.relu(self.fcs7(x))
+      return self.fcs8(x)
 
     def forward(self, x):
-    x = self.encode(x)
-    out = self.decode(x)
-    return out
+      latent = self.encode(x)
+      out = self.decode(latent)
+      return out
 
 # HNN autoencoder
-class PixelHNN(torch.nn.Module):
+class PixelHNN(nn.Module):
   def __init__(self, dims):
-    super(MLP_autoencoder, self).__init__()
+    super(PixelHNN, self).__init__()
     self.fcs1 = torch.nn.Linear(dims, 200)
     self.fcs2 = torch.nn.Linear(200, 200)
     self.fcs3 = torch.nn.Linear(200, 200)
@@ -78,19 +98,26 @@ class PixelHNN(torch.nn.Module):
     self.fcs7 = torch.nn.Linear(200, 200)
     self.fcs8 = torch.nn.Linear(200, dims)
 
-    def encode(self, x):
-    x = nn.Tanh((self.fcs1(x))
-    x = x + nn.Tanh(self.fcs2(x) 
-    x = x + nn.Tanh(self.fcs3(x))
+    self.relu = nn.ReLU()
+
+    self.hnn = HNN(dims=1)
+
+  def model_diff(self, x):
+    return self.hnn.diff_hnn(x)[1]
+
+  def encode(self, x):
+    x = self.relu(self.fcs1(x))
+    x = x + self.relu(self.fcs2(x))
+    x = x + self.relu(self.fcs3(x))
     return self.fcs4(x)
 
-    def decode(self, x):
-    x = nn.Tanh((self.fcs5(x))
-    x = x + nn.Tanh(self.fcs6(x) 
-    x = x + nn.Tanh(self.fcs7(x))
+  def decode(self, x):
+    x = self.relu(self.fcs5(x))
+    x = x + self.relu(self.fcs6(x))
+    x = x + self.relu(self.fcs7(x))
     return self.fcs8(x)
 
-    def forward(self, x):
-    x0 = self.encode(x)
-    out = # x0 + dx0/dt kéne h ez legyen, ide kéne a model deriválás
+  def forward(self, x):
+    latent = self.encode(x)
+    out = latent + self.model_diff(latent)
     return self.decode(out)
